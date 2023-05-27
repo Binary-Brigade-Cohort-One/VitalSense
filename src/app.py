7445e0afe31235723d17652a250feb09602b0896
Rabindra Suwal <rabindrasuwal8@gmail.com>
2023-05-27 16:17:26 -0400

    Added a new route for user page

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from markupsafe import escape
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'<h1>Hello, {escape(username)}!</h1>' 
+
