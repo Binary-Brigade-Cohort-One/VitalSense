744c42787bdceba5f5cbffe2eefde48e8ab549e5
Abhishek <poudelabhishek24@gmail.com>
2023-09-11 18:08:11 -0400

    Most of the logic completed

--- src/models.py
+++ src/models.py
@@ -8,30 +8,35 @@ from flask_login import LoginManager
 class USER(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(200), nullable=False)
+    image_file= db.Column(db.String(20), nullable=False, default='default.jpg')
     lastname = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     datas = db.relationship('DATA')
+    weekli = db.relationship('Weekly')
+
 
 
 class DATA(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    Height = db.Column(db.Integer, nullable=False)
-    Weight = db.Column(db.Integer, nullable=False)
-    Calorie_burned = db.Column(db.Integer, nullable=False)
+    Height = db.Column(db.Float, nullable=False)
+    Weight = db.Column(db.Float, nullable=False)
+    Calorie_burned = db.Column(db.Float, nullable=False)
     sleep_quality = db.Column(db.String(200), nullable=False)
     Heartrate = db.Column(db.String(200), nullable=False)
-    Caloriintake = db.Column(db.Integer, nullable=False)
-    Hydration = db.Column(db.Integer, nullable=False)
+    Caloriintake = db.Column(db.Float, nullable=False)
+    Hydration = db.Column(db.Float, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
 class Weekly(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    weeklyCalorie= db.Column(db.Integer, nullable=False)
+    weeklyCalorie= db.Column(db.Float, nullable=False)
     weeklyHydration= db.Column(db.Float, nullable=False)
     weeklyWeight=db.Column(db.Float, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 with app.app_context():  # we have to include this to make a table
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,13 @@
-from flask import redirect, render_template, url_for, request, flash
-
+from flask import redirect, render_template, url_for, request, flash, send_from_directory
 from src import app
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from src.models import USER , DATA, Weekly
 from src import db
-
+from werkzeug.utils import secure_filename
+import uuid as uuid
+import os
+from src import ALLOWED_EXTENSIONS
 
 
 
@@ -22,6 +24,7 @@ def createaccount(a):
         lastnamehtml = request.form['lastname']
         emailhtml = request.form['email']
         passwordhtml = request.form['password']
+        re_passwordhtml=request.form['repassword']
 
         if len(firstnamehtml) < 3:
             flash('frist name must be grater than 3 character', category="error")
@@ -32,6 +35,8 @@ def createaccount(a):
             flash("Email should be grater than 6 characters", category='error')
         elif len(passwordhtml) < 4:
             flash("Password should be grater than 4 characters", category="error")
+        elif (re_passwordhtml != passwordhtml):
+            flash("Password did not match!!", category="error")
         else:
 
             user = USER(firstname=firstnamehtml, lastname=lastnamehtml,
@@ -84,7 +89,7 @@ def login(a):
 
             if (b == True):
                 alluser = USER.query.all()
-                return redirect(url_for('user', a=1))
+                return redirect(url_for('user', a=0))
             else:
                 alluser = USER.query.all()
                 return  render_template('login.html', alluser=alluser, a=-1)
@@ -107,6 +112,7 @@ def show():
 @app.route('/user/<int:a>', methods=['POST', 'GET'])
 @login_required
 def user(a):
+    image_file=url_for('static', filename='profile_pics/'+ current_user.image_file)
     if (request.method == 'POST'):
         Heighthtml = request.form['height']
         Weighthtml = request.form['weight']
@@ -122,10 +128,10 @@ def user(a):
         db.session.commit()
         countdata=len(current_user.datas)
         if ( countdata% 7==0 and countdata !=0):   
-         return redirect(url_for('weekly', b=countdata))
+         return redirect(url_for('weekly', count=countdata))
          
         
-    return render_template('user.html', user=current_user, a=a)
+    return render_template('user.html', user=current_user, a=a, image_file=image_file)
 
 
 @app.route('/logout')
@@ -138,11 +144,11 @@ def logout():
 @app.route('/delete/<int:id>')
 @login_required
 def delete(id):
-    print(id)
-    if id %7 ==0:
-     weeklytotal=Weekly.query.all() 
+    if len(current_user.datas) %7 ==0:
+     weeklytotal=current_user.weekli 
      weektotal=len(weeklytotal)
-     week=Weekly.query.filter_by(id=weektotal).first()
+     print(weektotal-1)
+     week=current_user.weekli[weektotal-1]
      db.session.delete(week)
      db.session.commit()
          
@@ -182,22 +188,95 @@ def update(id):
     data = DATA.query.filter_by(id=id).one()
     return render_template('Update.html', data=data, id=id)
 
-@app.route('/weekly/<int:b>')
+@app.route('/weekly/<int:count>')
 @login_required
-def weekly(b):
- weekly_calories=0
- weekly_weight=0
- weekly_hydration=0
- i=b-6
- data=DATA.query.filter_by(id=i).one()
- averageidealWt=data.Weight*7
- for i in range(i, len(current_user.datas)+1):
-        data=DATA.query.filter_by(id=i).one()
-        weekly_calories += (data.Caloriintake)-(data.Calorie_burned)
-        weekly_weight += (data.Weight)
-        weekly_hydration += (data.Hydration)
+def weekly(count):
+ if(count !=1):
+     weekly_calories=0
+     weekly_weight=0
+     weekly_hydration=0
+     i=count-7
+     data=current_user.datas[i]
+     averageidealWt=(data.Weight)*7
+     for j in range(i, len(current_user.datas)):
+         data=current_user.datas[j]
+         weekly_calories += (data.Caloriintake)-(data.Calorie_burned)
+         weekly_weight += (data.Weight)
+         weekly_hydration += (data.Hydration)
+            
+     week= Weekly(weeklyCalorie= weekly_calories, weeklyWeight=weekly_weight, weeklyHydration=weekly_hydration, user_id=current_user.id)
+     db.session.add(week)
+     db.session.commit()
+     return render_template('weekly.html', week=week, user=current_user, avgidealWt=averageidealWt)
+
+ else:
+     if(len(current_user.weekli)==0):
+         return redirect(url_for('user', a=1))
+     return render_template('weekly.html', count =count, user=current_user)
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/profile',  methods=['GET', "POST"])
+@login_required
+def profile():
+    image= current_user.image_file
+    print(image)
+    if request.method == "POST":
+        firstnamehtml = request.form['firstname']
+        lastnamehtml = request.form['lastname']
+        emailhtml = request.form['email']
         
- week= Weekly(weeklyCalorie= weekly_calories, weeklyWeight=weekly_weight, weeklyHydration=weekly_hydration)
- db.session.add(week)
- db.session.commit()
- return render_template('weekly.html', week=week, user=current_user, avgidealWt=averageidealWt)
+        
+        if 'file' not in request.files:
+            flash('No file part', category="error")
+        
+        image_file=request.files['file']
+        print(image_file)
+        
+        if  image_file and allowed_file( image_file.filename):
+            filename = secure_filename( image_file.filename)
+            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            image_file=filename
+        else:
+            image_file=image    
+            
+        
+        
+        allemail=[user.email for user in USER.query.all()]
+        firstname=current_user.firstname
+        lastname=current_user.lastname
+        email=current_user.email
+
+
+        if len(firstnamehtml) < 3:
+            flash('frist name must be grater than 3 character', category="error")
+        elif len(lastnamehtml) < 1:
+            flash("lastname must be grater than 1 characters", category="error")
+        elif len(emailhtml) < 6:
+            flash("Email should be grater than 6 characters", category='error')
+        else:
+         if(firstnamehtml !=current_user.firstname or lastnamehtml != current_user.lastname or emailhtml !=current_user.email or image_file !=image):
+            
+                if(emailhtml in allemail and emailhtml !=current_user.email ):
+                 flash("Email already used! please Use new email!", category='error')
+                else:
+                 user=USER.query.filter_by(id=current_user.id).one()
+                 user.firstname=firstnamehtml
+                 user.lastname=lastnamehtml
+                 user.email=emailhtml
+                 user.image_file=image_file
+                    
+                 db.session.add(user)
+                 db.session.commit()
+                 if(image!=image_file or firstnamehtml !=firstname or lastnamehtml != lastname or emailhtml !=email):
+                     flash('Update successful!', category='success')
+                
+            
+    image_file=url_for('static', filename='profile_pics/'+ current_user.image_file)
+    return render_template("account.html", image_file=image_file, user=current_user)
+ 
+
